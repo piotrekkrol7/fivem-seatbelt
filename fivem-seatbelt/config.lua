43affe5f4f475b70719c4a53ac2d1e639618d54c
peterZ <piotrek-krol7@wp.pl>
2018-08-15 14:37:53 +0200

    update config.cfg
    zmiana wartości min speed.

--- fivem-seatbelt/config.lua
+++ fivem-seatbelt/config.lua
@@ -1,4 +1,4 @@
 Cfg             = {}
 Cfg.DiffTrigger = 0.255 
-Cfg.MinSpeed    = 19.25 --THIS IS IN m/s
+Cfg.MinSpeed    = 15.25 --THIS IS IN m/s
 Cfg.Strings     = { belt_on = 'Pasy bezpieczeństwa ^5 zapięte^0.', belt_off = 'Pasy bezpieczeństwa ^1 odpięte^0.' }
