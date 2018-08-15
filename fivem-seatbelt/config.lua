f82ec754ceb3299f5b178629f00871464ffa1f88
peterZ <piotrek-krol7@wp.pl>
2018-08-15 14:35:50 +0200

    Update config.lua

--- fivem-seatbelt/config.lua
+++ fivem-seatbelt/config.lua
@@ -1,4 +1,4 @@
 Cfg             = {}
 Cfg.DiffTrigger = 0.255 
 Cfg.MinSpeed    = 19.25 --THIS IS IN m/s
-Cfg.Strings     = { belt_on = 'Turvavyö ^5 kiinni^0.', belt_off = 'Turvavyö ^1 auki^0.' }
+Cfg.Strings     = { belt_on = 'Pasy bezpieczeństwa ^5 zapięte^0.', belt_off = 'Pasy bezpieczeństwa ^1 odpięte^0.' }
